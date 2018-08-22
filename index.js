77fa4e1bfaa748fdc1a205768e23d68d0645f867
jbb341@hotmail.com <jbb341@hotmail.com>
2018-08-22 17:46:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Add your functions and code here
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(kittens) {
+function destructivelyAppendKitten(kittens, Ralph) {
   kittens.push("Ralph")
-
+  return kittens
 }
